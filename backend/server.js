63db9ce79b99d79603c034f588deec5c837bb861
Vishnu25-07-1998 <156580998+Vishnu25-07-1998@users.noreply.github.com>
2025-03-22 12:53:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: process.env.CORS_ORIGIN || ["http:13.55.193.238:4000"],
+        origin: process.env.CORS_ORIGIN || ["http://13.55.193.238:4000"],
         methods: ["POST", "GET", "DELETE", "PUT"],
         credentials: true
     }
